6d0147a32bcdbec924c37981dc264613a8604f1a
Ahsan Ahmed <162038567+AhsXan@users.noreply.github.com>
2024-03-20 08:28:12 +0000

    2 new projects done

--- project5/pro5.js
+++ project5/pro5.js
@@ -1,1 +1,21 @@
-console.log("ASDADF")
+
+const insert = document.querySelector("#insert")
+
+window.addEventListener("keydown",(e)=> {
+   
+    insert.innerHTML= 
+   `<div id = "color"> 
+    <table>
+    <tr>
+      <th>Key</th>
+      <th>KeyCode</th>
+      <th>Code</th>
+    </tr>
+    <tr>
+      <td>${e.key}</td>
+      <td>${e.keyCode}</td>
+      <td>${e.code}</td>
+    </tr>
+  </table>  
+    </div>
+    `})
